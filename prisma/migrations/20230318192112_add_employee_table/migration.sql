21e2fa8587ccee9cbccd68f06d6ad7c1003751ee
Cassiano Junior <cassianojuniorww@gmail.com>
2023-03-18 16:21:34 -0300

    fix(prisma): Fix prisma migrations

--- prisma/migrations/20230318192112_add_employee_table/migration.sql
+++ prisma/migrations/20230318192112_add_employee_table/migration.sql
@@ -2,14 +2,19 @@
 CREATE TABLE "employees" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phoneNumber" TEXT NOT NULL,
     "salary" DOUBLE PRECISION NOT NULL,
     "jobTitle" TEXT NOT NULL,
     "profilePicture" TEXT,
+    "hiringDate" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "employees_cpf_key" ON "employees"("cpf");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "employees_email_key" ON "employees"("email");
